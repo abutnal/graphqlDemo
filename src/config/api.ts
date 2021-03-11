d72e15754588e7c40b9fa39c7b3532c39c2b8cf2
AB UTNAL <45108373+abutnal@users.noreply.github.com>
2021-03-11 06:44:57 +0530

    Add files via upload

---
+++ src/config/api.ts
@@ -0,0 +1,10 @@
+// CUSTOMER---------------------------------------------------------------
+export const CREATE_CUSTOMER = "createCustomer";
+export const UPDATE_CUSTOMER = "updateCustomer";
+export const DELETE_CUSTOMER = "deleteCustomer";
+export const GET_CUSTOMER = "getCustomer";
+export const GET_CUSTOMERS = "getCustomers";
+
+
+// PRODUCT---------------------------------------------------------------
+export const CREATE_PRODUCT = "createProduct";
